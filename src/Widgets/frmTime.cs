2b45947a9fbfd11b7bf4c426cd08c1da1577ebc1
Byron <53088136+byronbytes@users.noreply.github.com>
2021-11-21 10:40:03 -0500

    Widget Shape has been added

--- src/Widgets/frmTime.cs
+++ src/Widgets/frmTime.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,24 @@ namespace Glance.src.Widgets
 {
     public partial class frmTime : Form
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+             (
+                 int nLeftRect,     // x-coordinate of upper-left corner
+                 int nTopRect,      // y-coordinate of upper-left corner
+                 int nRightRect,    // x-coordinate of lower-right corner
+                 int nBottomRect,   // y-coordinate of lower-right corner
+                 int nWidthEllipse, // width of ellipse
+                 int nHeightEllipse // height of ellipse
+             );
         public frmTime()
         {
             InitializeComponent();
+            if(Properties.Settings.Default.WidgetShape == "newer")
+            {
+                Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+            }
+           
         }
 
         private void timer1_Tick(object sender, EventArgs e)
--- src/frmOptions.cs
+++ src/frmOptions.cs
@@ -20,24 +20,19 @@ namespace Glance.src
         private void frmOptions_Load(object sender, EventArgs e)
         {
             label4.Text = "Current Setting: " + Properties.Settings.Default.WidgetShape;
-           
-            
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            if (radioButton1.Checked == true)
-            {
-                Properties.Settings.Default.WidgetShape = "default";
-            }
-            if (radioButton2.Checked == true)
-            {
-                Properties.Settings.Default.WidgetShape = "newer";
-            }
-
+            WidgetShapeSetting();
         }
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            WidgetShapeSetting();
+        }
+
+        private void WidgetShapeSetting()
         {
             if (radioButton1.Checked == true)
             {
@@ -46,6 +41,19 @@ namespace Glance.src
             if (radioButton2.Checked == true)
             {
                 Properties.Settings.Default.WidgetShape = "newer";
+            }
+            label4.Text = "Current Setting: " + Properties.Settings.Default.WidgetShape;
+
+
+
+            if(Properties.Settings.Default.WidgetShape == "default")
+            {
+                radioButton1.Checked = true;
+            }
+
+            if(Properties.Settings.Default.WidgetShape == "newer")
+            {
+                radioButton2.Checked = true;
             }
         }
     }
